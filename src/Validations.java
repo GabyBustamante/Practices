0b9e6bd9ef24a46949ceb9f20c033b3fc1f7c6de
Gabriel <63618913+GabyBustamante@users.noreply.github.com>
2023-02-16 18:17:17 -0300

    Se agrega java docs

--- src/Validations.java
+++ src/Validations.java
@@ -1,10 +1,20 @@
 public class Validations {
     String numero;
 
+    /**
+     * Metodo para validacion de caracteres numerícos.
+     * @param numero
+     * @return boolean
+     */
     public static boolean isNumber (String numero){
         return numero.matches("[0-9]*");
     }
 
+    /**
+     * Metodo para validación de cadena de caracteres alfabéticos solamente
+     * @param string
+     * @return boolean
+     */
     public static boolean isString (String string){
         return string.matches("[a-zA-Z]*");
     }
